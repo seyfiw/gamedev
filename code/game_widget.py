db0365b4ec8368ffe8cc0c04cb82010995cae681
Chabil Satsanaphiban <6710110088@psu.ac.th>
2025-02-28 00:13:32 +0700

    animations

--- code/game_widget.py
+++ code/game_widget.py
@@ -10,7 +10,7 @@ class GameWidget(Widget):
         super().__init__(**kwargs)
         self.keyboard = KeyboardManager(self)
         self.map = Map("../image/map1-4.png")
-        self.player = Player("../image/removed-background.png", self.map)
+        self.player = Player("../image/Wr1.png", self.map)
         self.camera = Camera(self.map, self.player)
 
         self.add_widget(self.map.background)
@@ -24,10 +24,19 @@ class GameWidget(Widget):
         Clock.schedule_interval(self.update, 1/60)
 
     def update(self, dt):
-        self.player.update(dt, self.keyboard.pressed_keys)
+    
+        if "walk_left" in self.keyboard.pressed_keys:
+            self.player.move_left()  
+        elif "walk_right" in self.keyboard.pressed_keys:
+            self.player.move_right() 
+        else:
+            self.player.idle()  
+
+        self.player.update(dt, self.keyboard.pressed_keys)  
         self.camera.update()
 
         self.player.sprite.pos = (
             self.player.position[0] + self.map.background.pos[0],
             self.player.position[1] + self.map.background.pos[1]
-        )
+    )
+
--- code/player.py
+++ code/player.py
@@ -1,11 +1,25 @@
 from kivy.uix.image import Image
 from kivy.vector import Vector
+from kivy.clock import Clock
 
 class Player:
     def __init__(self, image_path, game_map):
         self.sprite = Image(source=image_path, size=(64, 64))
         self.position = [game_map.size[0] // 2, game_map.size[1] // 2]  
         self.game_map = game_map
+        self.animations = {
+            "walk_left": ["../image/Wl1.png", "../image/Wl1.png","../image/Wl1.png","../image/Wl1.png","../image/Wl1.png",
+                          "../image/Wl2.png", "../image/Wl3.png", "../image/Wl4.png", "../image/Wl4.png"
+                          , "../image/Wl4.png", "../image/Wl3.png","../image/Wl2.png",
+                          "../image/Wl1.png","../image/Wl1.png","../image/Wl1.png","../image/Wl1.png","../image/Wl1.png"],
+            "walk_right": ["../image/Wr1.png","../image/Wr1.png","../image/Wr1.png", "../image/Wr2.png", "../image/Wr3.png", "../image/Wr4.png", "../image/Wr4.png"
+                           , "../image/Wr4.png", "../image/Wr3.png","../image/Wr2.png","../image/Wr1.png","../image/Wr1.png","../image/Wr1.png","../image/Wr1.png"],
+        }
+        self.current_animation = "walk_left"  
+        self.current_frame = 0
+        self.animation_interval = 0.1 
+        Clock.schedule_interval(self.update_animation, self.animation_interval)
+
 
     def update(self, dt, pressed_keys):
         step_size = Vector(500 * dt, 500 * dt)
@@ -17,8 +31,12 @@ class Player:
             new_position -= Vector(0, step_size.y)
         if "a" in pressed_keys:
             new_position -= Vector(step_size.x, 0)
+            self.move_left() 
+            self.facing_left = True
         if "d" in pressed_keys:
             new_position += Vector(step_size.x, 0)
+            self.move_right()  
+            self.facing_left = False
 
         player_size = self.sprite.size
         check_points = [
@@ -36,3 +54,16 @@ class Player:
                 max(0, min(self.game_map.size[0] - player_size[0], new_position[0])),
                 max(0, min(self.game_map.size[1] - player_size[1], new_position[1]))
             ]
+            
+    def update_animation(self, dt):
+        self.sprite.source = self.animations[self.current_animation][self.current_frame]
+        self.current_frame = (self.current_frame + 1) % len(self.animations[self.current_animation])  
+
+    def move_left(self):
+        self.current_animation = "walk_left"
+
+    def move_right(self):
+        self.current_animation = "walk_right" 
+        
+    def idle(self):
+        pass
