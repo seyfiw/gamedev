647c7b53d2236d7786349e33e03f8362abbd8c13
สิทธิกร พินทอง <sittikornpinthong@gmail.com>
2025-03-05 01:52:25 +0700

    test

--- code/main.py
+++ code/main.py
@@ -1,5 +1,8 @@
 from kivy.app import App
 from game_widget import GameWidget
+from monster import Monster
+
+
 
 class MyGameApp(App):
     def build(self):
---
+++ code/monster.py
@@ -0,0 +1,40 @@
+import random
+from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle
+from kivy.core.image import Image as CoreImage
+
+class Monster(Widget):
+    def __init__(self, screen_width, screen_height, **kwargs):
+        super().__init__(**kwargs)
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.image = CoreImage("monster.png")
+        self.size = (self.image.width, self.image.height)
+        self.randomize_position()
+        with self.canvas:
+            self.rect = Rectangle(texture=self.image.texture, pos=self.pos, size=self.size)
+
+    def randomize_position(self):
+        self.pos = (random.randint(0, self.screen_width - self.width),
+                    random.randint(0, self.screen_height - self.height))
+        self.rect.pos = self.pos
+
+    def on_pos(self, instance, value):
+        self.rect.pos = value
+
+# Example usage:
+# from kivy.app import App
+# from kivy.uix.floatlayout import FloatLayout
+#
+# class MonsterApp(App):
+#     def build(self):
+#         screen_width = 800
+#         screen_height = 600
+#         layout = FloatLayout()
+#         monster = Monster(screen_width, screen_height)
+#         layout.add_widget(monster)
+#         return layout
+#
+# if __name__ == '__main__':
+#     MonsterApp().run()
+# monster.draw(screen)
